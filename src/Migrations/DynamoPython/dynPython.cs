f9131c9d10bd145e7b7a49bc3c6303cb7856961d
rk_lakshmanan <rk_lakshmanan@hotmail.com>
2016-06-29 14:49:47 +0800

    fixed namespace issue for python nodes

--- src/Migrations/DynamoPython/dynPython.cs
+++ src/Migrations/DynamoPython/dynPython.cs
@@ -3,37 +3,34 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml;
 
-namespace Dynamo.Nodes
+
+namespace DSIronPythonNode
 {
-
-    namespace DSIronPythonNode
+    public class PythonNode : MigrationNode
     {
-        public class PythonNode : MigrationNode
+        [NodeMigration(from: "0.8.3.0", to: "0.9.0.0")]
+        public static NodeMigrationData Migrate_0830_to_0900(NodeMigrationData data)
         {
-            [NodeMigration(from: "0.8.3.0", to: "0.9.0.0")]
-            public static NodeMigrationData Migrate_0830_to_0900(NodeMigrationData data)
-            {
-                System.Xml.XmlElement xmlNode = data.MigratedNodes.ElementAt(0);
-                var element = MigrationManager.CloneAndChangeName(xmlNode, "PythonNodeModels.PythonNode", "Python Script", true);
+            System.Xml.XmlElement xmlNode = data.MigratedNodes.ElementAt(0);
+            var element = MigrationManager.CloneAndChangeName(xmlNode, "PythonNodeModels.PythonNode", "Python Script", true);
 
-                var migrationData = new NodeMigrationData(data.Document);
-                migrationData.AppendNode(element);
-                return migrationData;
-            }
+            var migrationData = new NodeMigrationData(data.Document);
+            migrationData.AppendNode(element);
+            return migrationData;
         }
+    }
 
-        public class PythonStringNode : MigrationNode
+    public class PythonStringNode : MigrationNode
+    {
+        [NodeMigration(from: "0.8.3.0", to: "0.9.0.0")]
+        public static NodeMigrationData Migrate_0830_to_0900(NodeMigrationData data)
         {
-            [NodeMigration(from: "0.8.3.0", to: "0.9.0.0")]
-            public static NodeMigrationData Migrate_0830_to_0900(NodeMigrationData data)
-            {
-                System.Xml.XmlElement xmlNode = data.MigratedNodes.ElementAt(0);
-                var element = MigrationManager.CloneAndChangeName(xmlNode, "PythonNodeModels.PythonStringNode", "Python Script From String");
+            System.Xml.XmlElement xmlNode = data.MigratedNodes.ElementAt(0);
+            var element = MigrationManager.CloneAndChangeName(xmlNode, "PythonNodeModels.PythonStringNode", "Python Script From String");
 
-                var migrationData = new NodeMigrationData(data.Document);
-                migrationData.AppendNode(element);
-                return migrationData;
-            }
+            var migrationData = new NodeMigrationData(data.Document);
+            migrationData.AppendNode(element);
+            return migrationData;
         }
     }
 }
